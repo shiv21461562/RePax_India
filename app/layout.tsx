7ca686279acbe12f55395875a4e9d71c4a8376c9
Shiv <Shiv.k@founderCodes.com>
2026-06-15 15:37:23 +0530

    contact and sponsors page update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,21 @@
+import type { ReactNode } from "react";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import WhatsappButton from "@/components/layout/WhatsappButton";
 import "./globals.css";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
         <Navbar />
         {children}
-           <Footer />
-              <WhatsappButton />
+        <Footer />
+        <WhatsappButton />
       </body>
     </html>
   );
